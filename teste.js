c8b6fd2cf878ad77c0c4417477d11df8169c2446
nsjunior <nelsonsoujunior@gmail.com>
2018-03-31 02:37:09 -0300

    Delete teste.js

--- teste.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-var app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Olá mundo\n')
-})
- 
-app.listen(3000)
-console.log('Servidor iniciado em localhost:3000. Ctrl+C para encerrar...');
